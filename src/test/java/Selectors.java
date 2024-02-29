46c7e3142456ea74bf88e5b0661c9d0c7502f0f8
annagolod <anna.golod@gmail.com>
2024-02-29 20:44:11 +0200

    Added libraries Selenium & TestNG

---
+++ src/test/java/Selectors.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Selectors {
+WebDriver driver;
+    @Test
+    public void startTrello(){
+driver = new ChromeDriver();
+driver.get("https://trello.com");
+    }
+}
