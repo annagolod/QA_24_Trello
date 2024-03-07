9bee98bc6ab29ceda7869b3ba77804bea764ab7d
annagolod <anna.golod@gmail.com>
2024-03-07 20:28:58 +0200

    added startTrello Xpath

--- src/test/java/Selectors.java
+++ src/test/java/Selectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -8,5 +10,18 @@ WebDriver driver;
     public void startTrello(){
 driver = new ChromeDriver();
 driver.get("https://trello.com");
+WebElement btnLogin = driver.findElement(By.xpath("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']"));
+System.out.println("--> " + btnLogin.getText());
+
+WebElement getTrelloForFree = driver.findElement(By.xpath("//*[text()='Get Trello for free']"));
+System.out.println("--> " + getTrelloForFree.getAttribute("href"));
+
+WebElement btnSignUp = driver.findElement(By.xpath("//form[@data-testid='ui-email-signup-form']/div/button"));
+System.out.println("--> " + btnSignUp.getAttribute("aria-label"));
+
+
+
+
+        driver.quit();
     }
 }
