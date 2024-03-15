249fdc087208d725494646555c0e45d76c03fb93
annagolod <anna.golod@gmail.com>
2024-03-15 11:17:14 +0200

    added method loginPositivetest + Assert

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,6 +3,8 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class ApplicationManager {
 
     WebDriver driver;
@@ -12,6 +14,8 @@ public class ApplicationManager {
         driver = new ChromeDriver();
         driver.navigate().to("https://trello.com");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        helperUser = new HelperUser(driver);
 
     }
 
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends  TestBase {
 
     @Test
     public void loginPositiveTest(){
-
+        String email = "tretam0810@gmail.com";
+        String password = "Trello54321#";
+        app.getHelperUser().login(email, password);
+        Assert.assertTrue(app.getHelperUser().isElementPresent_btnAccount());
     }
 
 
