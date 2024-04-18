b822ef56cddffc91fbeb8ec19998cf9864b4f52f
annagolod <anna.golod@gmail.com>
2024-04-18 20:11:06 +0300

    fixed deleteBoardtest

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -30,6 +33,7 @@ public class ApplicationManager {
 //        options.addArguments("--lang=en");
 //        driver = new EventFiringWebDriver(new ChromeDriver());
         if(browser.equals(BrowserType.FIREFOX)){
+            System.setProperty("webdriver.firefox.bin", "/Applications/Firefox.app/Contents/MacOS/firefox");
             driver = new EventFiringWebDriver(new FirefoxDriver());
             logger.info("testing starts on Firefox");
         }else{
@@ -46,6 +50,7 @@ public class ApplicationManager {
         driver.register(new WDListener());
         helperProfile = new HelperProfile(driver);
     }
+
 
     public void stop() {
         logger.info("Stop testing -- navigate to --> https://trello.com");
--- src/test/java/tests/BoardsTests.java
+++ src/test/java/tests/BoardsTests.java
@@ -16,7 +16,7 @@ import java.util.Random;
 
 @Listeners(TestNGListener.class)
 
-public class BoardsTests extends TestBase{
+public class BoardsTests extends TestBase {
 
 //    @BeforeClass(alwaysRun = true)
 //    public void loginWithData(){
@@ -24,7 +24,7 @@ public class BoardsTests extends TestBase{
 //    }
 
     @Test(groups = {"smoke"}, dataProvider = "DP_createNewBoardPositiveTest", dataProviderClass = DataProviderBoard.class)
-    public void createNewBoardPositiveTest(Method method, BoardDTO boardDTO){
+    public void createNewBoardPositiveTest(Method method, BoardDTO boardDTO) {
 //        int i = new Random().nextInt(1000);
 //        BoardDTO boardDTO = BoardDTO.builder()
 //                .boardTitle("qa24_" + i)
@@ -33,35 +33,41 @@ public class BoardsTests extends TestBase{
         logger.info("start test " + method.getName() + " board title--> " + boardDTO.getBoardTitle());
         app.getHelperBoards().createNewBoard(boardDTO);
         Assert.assertTrue(app.getHelperBoards().isTextInElementEquals_boardTitle(boardDTO.getBoardTitle()));
+
     }
+
     @Test(expectedExceptions = TimeoutException.class)
-    public void createNewBoardNegativeTest_EmptyBoardTitle(){
+    public void createNewBoardNegativeTest_EmptyBoardTitle() {
         BoardDTO boardDTO = BoardDTO.builder()
                 .boardTitle("  ")
                 .build();
         app.getHelperBoards().createNewBoard(boardDTO);
         Assert.assertTrue(app.getHelperBoards().isElementPresent_inputBoardTitle());
+
     }
+
     @Test//(retryAnalyzer = RetryAnalyzer.class)
-    public void deleteBoardPositiveTest(Method method){
+    public void deleteBoardPositiveTest(Method method) {
         int i = new Random().nextInt(1000);
         BoardDTO boardDTO = BoardDTO.builder()
                 .boardTitle("qa24_" + i)
                 .build();
         logger.info("start test " + method.getName() + " board title--> " + boardDTO.getBoardTitle());
         app.getHelperBoards().createNewBoard(boardDTO);
+        app.getHelperBoards().pause(5);
         app.getHelperBoards().clickBtnBoard();
+        //app.getHelperBoards().pause(5);
         app.getHelperBoards().deleteBoard(boardDTO);
         Assert.assertTrue(app.getHelperBoards().textToBePresentInElement_BoardDeleted("Board deleted.", 5));
 
     }
 
-@AfterMethod(alwaysRun = true)
-    public void afterTest(){
-       if(app.getHelperBoards().isElementPresent_boardTitle())
-           app.getHelperBoards().clickBtnBoard();
-       if(app.getHelperBoards().isElementPresent_inputBoardTitle())
-           app.getHelperBoards().clickBtnCloseCreateForm();
-}
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        if (app.getHelperBoards().isElementPresent_boardTitle())
+            app.getHelperBoards().clickBtnBoard();
+        if (app.getHelperBoards().isElementPresent_inputBoardTitle())
+            app.getHelperBoards().clickBtnCloseCreateForm();
+    }
 
 }
